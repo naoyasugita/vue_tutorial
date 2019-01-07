32bca12a206dc78bf7619eb2a40c884b10423478
s_naoya <sugita6878@gmail.com>
2019-01-07 18:14:44 +0900

    はじめにまで終了

---
+++ official/index.js
@@ -0,0 +1,67 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'hello'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'hello'
+    }
+})
+
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'vegetables' },
+            { id: 1, text: 'cheese' },
+            { id: 2, text: 'whatever else humans are supported to eat' }
+        ]
+    }
+})
